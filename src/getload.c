ee2a9b0cd2ce61aa2fe1d5bba491fbaac321576d
Joe Wingbermuehle <joewing@joewing.net>
2014-02-22 22:31:52 -0600

    Fix build on IRIX.

--- src/getload.c
+++ src/getload.c
@@ -12,6 +12,9 @@
 #ifdef HAVE_SYS_LOADAVG_H
 #  include <sys/loadavg.h>
 #endif
+#ifdef HAVE_SYS_SYSGET_H
+#  include <sys/sysget.h>
+#endif
 
 double GetLoad()
 {
@@ -19,6 +22,14 @@ double GetLoad()
 
 #ifdef HAVE_GETLOADAVG
    getloadavg(&avg, 1);
+#elif defined(HAVE_SYSGET)
+   struct sgt_cookie cookie;
+   int load[3];
+   SGT_COOKIE_INIT(&cookie);
+   SGT_COOKIE_SET_KSYM(&cookie, KSYM_AVENRUN);
+   sysget(SGT_KSYM, (char*)&load, sizeof(load), SGT_READ, &cookie);
+   avg = (double)load[0] / 1024.0;
+   printf("%g\n", (float)avg);
 #else
 #  error "don't know how to get the load average"
 #endif
